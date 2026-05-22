a8800e31819a5cb6745cbe5bebe6b2a23bb4f86f
yabo-san <kevin@y4bo.com>
2026-05-21 19:54:27 -0700

    ghostty: fix quick-terminal gap on display change + AltTab defaults script

---
+++ run_onchange_setup-alttab.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# AltTab (com.lwouis.alt-tab-macos) — re-runs when this file changes
+
+defaults write com.lwouis.alt-tab-macos appearanceSize -int 2
+defaults write com.lwouis.alt-tab-macos appsToShow -int 0
+defaults write com.lwouis.alt-tab-macos spacesToShow -int 0
+defaults write com.lwouis.alt-tab-macos showHiddenWindows -int 1
+defaults write com.lwouis.alt-tab-macos showMinimizedWindows -int 0
+defaults write com.lwouis.alt-tab-macos previewFocusedWindow -bool true
+defaults write com.lwouis.alt-tab-macos menubarIconShown -bool true
+defaults write com.lwouis.alt-tab-macos updatePolicy -int 1
+
+# Exceptions: hide Finder + iTerm2 from switcher; ignore VM/remote-desktop apps
+defaults write com.lwouis.alt-tab-macos exceptions '[
+  {"ignore":"0","hide":"1","bundleIdentifier":"com.McAfee.McAfeeSafariHost"},
+  {"ignore":"0","hide":"2","bundleIdentifier":"com.apple.finder"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.microsoft.rdc.macos"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.teamviewer.TeamViewer"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"org.virtualbox.app.VirtualBoxVM"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.parallels."},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.citrix.XenAppViewer"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.citrix.receiver.icaviewer.mac"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.nicesoftware.dcvviewer"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.vmware.fusion"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.apple.ScreenSharing"},
+  {"ignore":"2","hide":"0","bundleIdentifier":"com.utmapp.UTM"},
+  {"ignore":"0","hide":"1","bundleIdentifier":"com.googlecode.iterm2"}
+]'
